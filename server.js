f3a54b44e4cf9938511c05b7e1f7fc2048a2165f
CavalieredelRe <sasasatore.ss@gmail.com>
2025-07-20 15:04:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,52 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Servi file statici dalla directory corrente
-app.use(express.static(__dirname));
+const PORT = process.env.PORT || 3000;
 
+// Servire i file statici dalla cartella "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route base: servire index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+let onlineUsers = {};
+
+io.on("connection", (socket) => {
+  let user = null;
+
+  socket.on("userConnected", (username) => {
+    user = username;
+    onlineUsers[user] = socket;
+  });
+
+  socket.on("chatMessage", (data) => {
+    for (let name in onlineUsers) {
+      if (name !== data.user) {
+        onlineUsers[name].emit("chatMessage", data);
+      }
+    }
+  });
+
+  socket.on("userDisconnected", () => {
+    if (user) delete onlineUsers[user];
+  });
+
+  socket.on("disconnect", () => {
+    if (user) delete onlineUsers[user];
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`✅ Server avviato sulla porta ${PORT}`);
+});
 io.on('connection', (socket) => {
   console.log('Un utente si è connesso');
 
