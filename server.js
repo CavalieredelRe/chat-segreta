18b7d6b7d3ad2027aae94905ece761d410976569
CavalieredelRe <sasasatore.ss@gmail.com>
2025-07-20 14:56:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,42 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const path = require("path");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Servi file statici dalla directory corrente
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+  console.log('Un utente si è connesso');
+
+  socket.on('chatMessage', (data) => {
+    socket.broadcast.emit('chatMessage', data);
+  });
+
+  socket.on('userConnected', (user) => {
+    socket.broadcast.emit('chatMessage', {
+      user: 'Sistema',
+      message: `${user} si è unito alla chat.`,
+    });
+  });
+
+  socket.on('userDisconnected', () => {
+    socket.broadcast.emit('chatMessage', {
+      user: 'Sistema',
+      message: `Un utente ha lasciato la chat.`,
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Utente disconnesso');
+  });
+});
+
 const PORT = process.env.PORT || 3000;
-
-// Serve file statici dalla cartella 'public'
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Rotta per la homepage
-app.get('/', (req, res) => {
-  /opt/render/project/src/public/index.html
-});
-
-// Traccia utenti connessi
-let onlineUsers = {};
-
-io.on("connection", (socket) => {
-  let user = null;
-
-  // Quando un utente si connette
-  socket.on("userConnected", (username) => {
-    user = username;
-    onlineUsers[user] = socket;
-    console.log(`🟢 ${user} connesso`);
-  });
-
-  // Quando riceve un messaggio
-  socket.on("chatMessage", (data) => {
-    for (let name in onlineUsers) {
-      if (name !== data.user) {
-        onlineUsers[name].emit("chatMessage", data);
-      }
-    }
-  });
-
-  // Quando l'utente si disconnette manualmente
-  socket.on("userDisconnected", () => {
-    if (user) {
-      delete onlineUsers[user];
-      console.log(`🔴 ${user} disconnesso manualmente`);
-    }
-  });
-
-  // Quando la connessione viene persa
-  socket.on("disconnect", () => {
-    if (user) {
-      delete onlineUsers[user];
-      console.log(`🔴 ${user} disconnesso`);
-    }
-  });
-});
-
-// Avvia il server
 server.listen(PORT, () => {
-  console.log(`🚀 Server avviato su http://localhost:${PORT}`);
+  console.log(`Server attivo su http://localhost:${PORT}`);
 });
