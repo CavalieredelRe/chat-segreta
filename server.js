cc02c997ca1b9dadf5f0290a3578139abe78b5fa
CavalieredelRe <sasasatore.ss@gmail.com>
2025-07-21 11:20:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,40 +9,58 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
+// Servire file statici dalla cartella "public"
 app.use(express.static(path.join(__dirname, "public")));
+
+// Serve index.html alla root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 let onlineUsers = {};
 
 io.on("connection", (socket) => {
   let user = null;
 
+  console.log("🔌 Nuova connessione");
+
   socket.on("userConnected", (username) => {
     user = username;
     onlineUsers[user] = socket;
+
+    socket.broadcast.emit("chatMessage", {
+      user: "Sistema",
+      message: `${user} si è unito alla chat.`
+    });
   });
 
   socket.on("chatMessage", (data) => {
-    // Broadcast to all users except the sender
-    for (let name in onlineUsers) {
-      if (name !== data.user) {
-        onlineUsers[name].emit("chatMessage", data);
-      }
-    }
+    socket.broadcast.emit("chatMessage", data);
   });
 
   socket.on("userDisconnected", (username) => {
-    if (user) delete onlineUsers[user];
+    if (user) {
+      delete onlineUsers[user];
+      socket.broadcast.emit("chatMessage", {
+        user: "Sistema",
+        message: `${user} ha lasciato la chat.`
+      });
+    }
   });
 
-  socket.on("disconnect", () => {
-    if (user) delete onlineUsers[user];
+  socket.on("disconnect", (username) => {
+    if (user) {
+      delete onlineUsers[user];
+      socket.broadcast.emit("chatMessage", {
+        user: "Sistema",
+        message: `${user} ha lasciato la chat.`
+      });
+    }
+    console.log("❌ Disconnessione");
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
+// ✅ Solo UNA chiamata a server.listen()
 server.listen(PORT, () => {
   console.log(`✅ Server attivo su http://localhost:${PORT}`);
 });
